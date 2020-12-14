3aa26ac361666e872ce42d4582d5b9b8b8758915
fabiomedda <meddafabio@gmail.com>
2020-12-14 16:57:22 +0100

    Cala Goloritzé

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data
+*/
+
+let root = new Vue({
+    el: "#root",
+    data: {
+        message: "Hello Cala Goloritzé",
+        img: "assets/img/arco_Cala_Goloritzé.jpg"
+    }
+});
